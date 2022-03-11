4169920057cfdee5c530e6d1b3c91946eb62f3c5
Documenter.jl <documenter@juliadocs.github.io>
2022-03-11 22:00:45 +0000

    build based on 8b48402

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ACEbonds","category":"page"},{"location":"#ACEbonds","page":"Home","title":"ACEbonds","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ACEbonds.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ACEbonds]","category":"page"}]
+}
