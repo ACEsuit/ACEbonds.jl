7fafb65453ed23f8c55dcda13bf177f03495bc1b
Christoph Ortner <christophortner0@gmail.com>
2022-05-03 20:57:02 -0700

    backup

--- src/localstate.jl
+++ src/localstate.jl
@@ -4,17 +4,22 @@ using ACE: State, filter
 
 struct BondsIterator 
    at
-   env
+   nlist_bond
+   rcutbond 
+   nlist_env 
+   rcutenv 
+   filter 
 end 
 
-bonds(at::Atoms, env::BondEnvelope) = BondsIterator(at, env)
+function bonds(at::Atoms, rcutbond, rcutenv, filter) 
+   nlist_bond = neighbourlist(at, rcutbond) 
+   nlist_env = neighbourlist(at, rcutenv)
+   return BondsIterator(at, nlist_bond, rcutbond, nlist_env, rcutenv, filter)
+end
 
 function iterate(iter::BondsIterator, state=(0,0))
-   at = iter.at
-   env = iter.env 
    i, q = state 
-   Js, _ = neigs(nlist, i)
-   Js = sort(Js) # sort in increasing order 
+   Js, Rs = neigs(iter.nlist_bond, i)
 
    # nothing left to do 
    if i >= length(at) && q >= length(Js)
@@ -24,71 +29,49 @@ function iterate(iter::BondsIterator, state=(0,0))
    # increment: 
    #   case 1: we haven't yet exhausted the current neighbours. 
    #           just increment the q index pointing into Js 
-   if j < length(Js)
+   if q < length(Js)
       q += 1
+      # here we could build in a rule to skip any pair for which we don't 
+      # want to do the computation. 
+
     #  case 2: if i < length(at) but q >= length(Js) then we need to 
     #          increment the i index and get the new neighbours 
    elseif 
       i += 1 
-      Js, _ = neigs(nlist, i)
+      Js, Rs = neigs(iter.nlist_bond, i)
       Js = sort(Js) # sort in increasing order 
       q = 1 
-      # skip all j = Js[q] for which we've already visited (i, j). Namely, if 
-      # r_ij <= rcut and j < i then we've already found the bond (i, j) when we 
-      # were sitting at j and looking at that neighbourhood. This should even 
-      # work without minimum image convention. 
-      # TODO: check this argument and then implement this skipping step!
    end 
 
-   j = Js[q]
+   j = Js[q]   # index of neighbour (in central cell)
+   rrj = Rs[q]  # position of neighbour (in shifted cell)
+   ssj = Rs[q] - at.X[j]   # shift of atom j into shifted cell
+   
+   # now we construct the environment 
+
    
 end
 
+function _get_bond(nlist, i, rri, j, rrj, filter)
+   Js_i, Rs_i = neigs(nlist, i)
 
-"""
-get_bond(at,i,j)
-input:  an Atoms at, atomic index i and j
-output: index of the nearest atom having index j, i.e., the one really forms a 
-        bond with atom i
-"""
-function get_bond(at::JuLIP.Atoms, i::Int64, j::Int64)
-   nlist = JuLIP.neighbourlist(at,20.0)
-   neigh_i = JuLIP.Potentials.neigsz(nlist,at,i)
-   idx = findall(isequal(j),neigh_i[1])
-   idx = idx[findmin(norm.(neigh_i[2][idx]))[2]]
-   return idx
-end
-
-"""
-get_state(at,i,j,env)
-input:  an Atoms at, atomic index i and j, and an local envelop env
-output: local state of the bond formed by atom i and j
-
-The variables i, j can be replaced by a vector of tuples, so that a set of states
-can be found.
-"""
-function get_state(at::JuLIP.Atoms,i::Int64,j::Int64,env::BondEnvelope; λ = 0)
-   if i==j
-      error("i,j should be distinct")
-   end
-   nlist = JuLIP.neighbourlist(at,20.0)
-   neigh_i = JuLIP.Potentials.neigsz(nlist,at,i)
-   idx = get_bond(at,i,j)
-   # rr - vector form of the bond
-   rr = neigh_i[2][idx]
-   # rr_c - centred 
-   rr_c = λ * rr
-   st = State(rr = rr-rr_c, rr0 = rr, be=:bond)
-   # TODO: include the atomic number
-   for (jj, rj) in enumerate(neigh_i[2])
-      st_temp = State(rr = rj-rr_c, rr0 = rr, be=:env)
-      if rj≠rr && filter(env,st_temp)
-         st = [st; st_temp]
+   rrmid = 0.5 * (rri + rrj)
+   rrbond = rrj - rri
+   env = [] 
+   for (q, rrq) in zip(Js_i, Rs_i) 
+      rr = rrq + rri - rrmid 
+      if rr ≈ rrj   # TODO: replace this with checking for j and shift!
+         @assert Js_i[q] == j 
+         X = State(rr = rrbond,  # or should it be zero? 
+                   rr0 = rrbond, 
+                   be = :bond)
+         push!(env, X)
+      elseif filter(rr)
+         X = State(rr = rr, 
+                   rr0 = rrbond, 
+                   be = :env)
+         push!(env, X)
       end
    end
-   return st
+   return identity.(env)
 end
-
-# get_state(at,index,env=CylindricalBondEnvelope(18.0,10.0,10.0)) = [ get_state(at,i,j,env) for (i,j) in index ]
-
-# TODO: convert to cylinderical coordinate? Will need a default direction of ϕ...
